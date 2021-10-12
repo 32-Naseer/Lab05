b9acb9e1f52fbd4a984d408b9124be73b78e4381
32-Naseer <91900098+32-Naseer@users.noreply.github.com>
2021-10-12 19:29:11 +0800

    Added Timer for lose condition and win condition

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CoinScript : MonoBehaviour
 {
     public Text scoreText;
-    private static int scoreCount;
+    public static int scoreCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +17,23 @@ public class CoinScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(scoreCount == 60)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
-            scoreCount += 10;
-            scoreText.text = "Score: " + scoreCount;
+            if (scoreCount != 60)
+            {
+                Destroy(gameObject);
+                scoreCount += 10;
+                scoreText.text = "Score: " + scoreCount;
+            }
         }
     }
+
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTimer();
+        CheckTimerLoseCondition();
+        timeText.text = DisplayTime(timeRemaining);
+    }
+
+    string DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        return $"{minutes:00}:{seconds:00}";
+    }
+
+    void CheckTimer()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void CheckTimerLoseCondition()
+    {
+        //1 = GameLoseScene
+        if (timeRemaining <= 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
